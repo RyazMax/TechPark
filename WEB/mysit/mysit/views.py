2a41f79624611ddd4788dc0ff898aa0cdcd34e1b
RyazMax <ryazanovms@yandex.ru>
2018-03-16 15:50:31 +0300

    Add WEB homework and django project

---
+++ WEB/mysit/mysit/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Главная страница со списком вопросов
+def showIndex(request):
+    return render(request, 'index.html')
+
+# Страница с одним вопросом
+def showQuestion(request):
+    return render(request, 'question.html')
+
+# Страница с новым вопросом
+def showAsk(request):
+    return render(request, 'ask.html')
+
+# Страница авторизации
+def showLogIn(request):
+    return render(request, 'login.html')
+
+# Страница регистрации
+def showSignUp(request):
+    return render(request, 'signup.html')
